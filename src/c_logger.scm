3b661221ec780ea9904fa5952d491fcc9e28bb8e
wehu <huwei04@hotmail.com>
2012-11-01 15:52:39 +0800

    refine  Changes to be committed: 	modified:   src/c_logger.scm

--- src/c_logger.scm
+++ src/c_logger.scm
@@ -9,30 +9,33 @@
   (use extras)
 
   (define (p . args)
-    (for-each (lambda (arg)
-      (format #t "~a " arg))
-      args)
-    (format #t "\n"))
+    (let ((ss (fold (lambda (arg init)
+                     (append init (list (format #f "~a" arg))))
+               init
+               args)))
+      (display "[nao ")
+      (display (string-join ss) " ")
+      (display "\n")))
 
   (define-external (info (nonnull-c-string arg)) void
     (info^ arg))
   (define (info^ . args)
-    (apply p (cons "[INFO]:" args)))
+    (apply p (cons "INFO]:" args)))
 
   (define-external (err (nonnull-c-string arg)) void
     (err^ arg))
   (define (err^ . args)
-    (apply p (cons "[ERR]:" args)))
+    (apply p (cons "ERR]:" args)))
 
   (define-external (warn (nonnull-c-string arg)) void
     (warn^ arg))
   (define (warn^ . args)
-    (apply p (cons "[WARN]:" args)))
+    (apply p (cons "WARN]:" args)))
 
   (define-external (debug (nonnull-c-string arg)) void
     (debug^ arg))
   (define (debug^ . args)
-    (apply p (cons "[DEBUG]:" args))))
+    (apply p (cons "DEBUG]:" args))))
 
 (import logger)
 
