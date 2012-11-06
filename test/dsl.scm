cf700cd9ed1ba01e6782947b8aa0e2a398da4382
wehu <huwei04@hotmail.com>
2012-11-06 18:02:46 +0800

    add dsl test  Changes to be committed: 	new file:   test/dsl.scm

---
+++ test/dsl.scm
@@ -0,0 +1,19 @@
+(import nao)
+
+(define chan (make-chan))
+(define ev (make-event))
+(define sig (make-sig))
+
+(always@ (chan ev sig)
+  (info ($))
+  (info ($ 1))
+  (info ($ 2))
+  (info ($ 3))
+  (info "====="))
+
+(initial
+  (!> sig "sig")
+  (@ 1)
+  (-> chan "chan")
+  (@ 1)
+  (notify ev))
